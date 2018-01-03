6f59525b4dae0a7e27a4f68f7613c16466beb2de
westdoorblowcola <510908220@qq.com>
2018-01-03 11:50:32 +0800

    Update cluster.py
    修复   ERROR (1264, "Out of range value for column 'repeats' at row 1")

--- django_q/cluster.py
+++ django_q/cluster.py
@@ -530,6 +530,11 @@ def scheduler(broker=None):
                         break
                 s.next_run = next_run.datetime
                 s.repeats += -1
+                
+                # rapair Out of range value for column 'repeats' at row 1
+                if s.repeats > 5000:
+                    s.repeats = -1
+                
             # send it to the cluster
             q_options['broker'] = broker
             q_options['group'] = q_options.get('group', s.name or s.id)
